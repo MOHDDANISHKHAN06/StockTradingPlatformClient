4232ecbee513e1b73e1fa66fa0353beb0a0c30a8
MOHDDANISHKHAN06 <danishkhn019@gmail.com>
2024-02-03 14:51:52 -0700

    local

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,29 +3,31 @@ import instance from "../helper/authheader";
 var user = JSON.parse(sessionStorage.getItem("user"));
 var emailId = sessionStorage.getItem("emailId");
 
-// const USER_API_BASE_URL = "http://localhost:8080/api/auth/signup";
-const USER_API_BASE_URL =
-  "http://stock-trading-platform.railway.internal/api/auth/signup"; // Updated URL
-// const ALL_USER_API_BASE_URL = `http://localhost:8080/api/users`;
-const ALL_USER_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users`; // Updated URL
-// const WALLET_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/wallet`;
-const WALLET_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/wallet`; // Updated URL
-// const TRANSACTION_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/transaction`;
-const TRANSACTION_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/transaction`; // Updated URL
-// const CANCEL_ORDER_CASE_URL = `http://localhost:8080/api/users/${emailId}/cancel`;
-const CANCEL_ORDER_CASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/cancel`; // Updated URL
-// const CHANGE_MKT_TIME_BASE_URL = "http://localhost:8080/api/market/changemarkethours";
+const USER_API_BASE_URL = "http://localhost:8080/api/auth/signup";
+// const USER_API_BASE_URL =
+//   "http://stock-trading-platform.railway.internal/api/auth/signup"; // Updated URL
+const ALL_USER_API_BASE_URL = `http://localhost:8080/api/users`;
+// const ALL_USER_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users`; // Updated URL
+const WALLET_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/wallet`;
+// const WALLET_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/wallet`; // Updated URL
+const TRANSACTION_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/transaction`;
+// const TRANSACTION_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/transaction`; // Updated URL
+const CANCEL_ORDER_CASE_URL = `http://localhost:8080/api/users/${emailId}/cancel`;
+// const CANCEL_ORDER_CASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/cancel`; // Updated URL
 const CHANGE_MKT_TIME_BASE_URL =
-  "http://stock-trading-platform.railway.internal/api/market/changemarkethours"; // Updated URL
-// const ADD_HOLIDAY_BASE_URL = "http://localhost:8080/api/market/addHoliday";
-const ADD_HOLIDAY_BASE_URL =
-  "http://stock-trading-platform.railway.internal/api/market/addHoliday"; // Updated URL
-// const GET_MKT_SCHEDULE_BASE_URL = "http://localhost:8080/api/marketschedule";
-const GET_MKT_SCHEDULE_BASE_URL =
-  "http://stock-trading-platform.railway.internal/api/marketschedule"; // Updated URL
-// const DELETE_HOLIDAY_BASE_URL = "http://localhost:8080/api/market/deleteHoliday";
+  "http://localhost:8080/api/market/changemarkethours";
+// const CHANGE_MKT_TIME_BASE_URL =
+// "http://stock-trading-platform.railway.internal/api/market/changemarkethours"; // Updated URL
+const ADD_HOLIDAY_BASE_URL = "http://localhost:8080/api/market/addHoliday";
+// const ADD_HOLIDAY_BASE_URL =
+// "http://stock-trading-platform.railway.internal/api/market/addHoliday"; // Updated URL
+const GET_MKT_SCHEDULE_BASE_URL = "http://localhost:8080/api/marketschedule";
+// const GET_MKT_SCHEDULE_BASE_URL =
+// "http://stock-trading-platform.railway.internal/api/marketschedule"; // Updated URL
 const DELETE_HOLIDAY_BASE_URL =
-  "http://stock-trading-platform.railway.internal/api/market/deleteHoliday"; // Updated URL
+  "http://localhost:8080/api/market/deleteHoliday";
+// const DELETE_HOLIDAY_BASE_URL =
+// "http://stock-trading-platform.railway.internal/api/market/deleteHoliday"; // Updated URL
 
 export class UserService {
   saveUser(user) {
