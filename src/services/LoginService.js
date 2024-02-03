14bfff045c9f194f82c251b2f42ab98af27043ee
MOHDDANISHKHAN06 <danishkhn019@gmail.com>
2024-02-03 14:52:19 -0700

    local

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const LOGIN_BASE_URL =  "http://localhost:8080/api/auth/signin";
-const LOGIN_BASE_URL =
-  "http://stock-trading-platform.railway.internal/api/auth/signin"; // Updated URL
+const LOGIN_BASE_URL = "http://localhost:8080/api/auth/signin";
+// const LOGIN_BASE_URL =
+//   "http://stock-trading-platform.railway.internal/api/auth/signin"; // Updated URL
 
 export class LoginService {
   async loginUser(email, password) {
