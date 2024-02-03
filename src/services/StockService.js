54e4752f33a54adfc08b16017291634ff5a7959e
MOHDDANISHKHAN06 <danishkhn019@gmail.com>
2024-02-03 15:08:21 -0700

    online url

--- src/services/StockService.js
+++ src/services/StockService.js
@@ -3,15 +3,15 @@ import instance from "../helper/authheader";
 // var user = JSON.parse(sessionStorage.getItem("user"));
 // var emailId = user !== null ? user.emailId : null;
 var emailId = sessionStorage.getItem("emailId");
-const STOCK_API_BASE_URL = "http://localhost:8080/api/stocks";
-// const STOCK_API_BASE_URL =
-// "http://stock-trading-platform.railway.internal/api/stocks"; // Updated URL
-const USERSTOCK_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/stocks`;
-// const USERSTOCK_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/stocks`; // Updated URL
-const ORDER_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/allorders`;
-// const ORDER_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/allorders`; // Updated URL
-const BUY_SELL_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/order`;
-// const BUY_SELL_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/order`; // Updated URL
+// const STOCK_API_BASE_URL = "http://localhost:8080/api/stocks";
+const STOCK_API_BASE_URL =
+  "http://stock-trading-platform.railway.internal/api/stocks"; // Updated URL
+// const USERSTOCK_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/stocks`;
+const USERSTOCK_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/stocks`; // Updated URL
+// const ORDER_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/allorders`;
+const ORDER_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/allorders`; // Updated URL
+// const BUY_SELL_API_BASE_URL = `http://localhost:8080/api/users/${emailId}/order`;
+const BUY_SELL_API_BASE_URL = `http://stock-trading-platform.railway.internal/api/users/${emailId}/order`; // Updated URL
 
 export class StockService {
   getStocks() {
