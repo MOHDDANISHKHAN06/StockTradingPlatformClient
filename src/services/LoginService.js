e7d6e949831debb0b19add5ff33ba11d7b2a290a
MOHDDANISHKHAN06 <danishkhn019@gmail.com>
2024-02-07 01:26:13 -0700

    updated URL

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -4,6 +4,7 @@ const LOGIN_BASE_URL =
   "http://Stp-server-2-env.eba-pctz3tq8.us-east-2.elasticbeanstalk.com/api";
 export class LoginService {
   async loginUser(email, password) {
+    console.log("Making request to:", LOGIN_BASE_URL); // Debug log
     return await axios.post(LOGIN_BASE_URL, {
       emailId: email,
       password: password,
